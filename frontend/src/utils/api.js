d19f71a892d943100bf878ec6d28991ca59e7ed9
Arunjangir8 <arunjangir9987@example.com>
2025-11-04 01:03:20 +0530

    6th

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5000/api';
+const HOST_BASE_URL = 'https://campus-connect-one-green.vercel.app/api'
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: HOST_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
